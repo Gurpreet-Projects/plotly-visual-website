151b8eff6b4c5e7f1966be96413bd425bcd63441
Gurpreet <deol.gs@gmail.com>
2019-10-02 12:17:09 -0700

    created index.html

---
+++ plotly-visual.py
@@ -0,0 +1,22 @@
+#%%
+# import required libraries
+import plotly.express as px
+
+
+#%%
+gapminder = px.data.gapminder()
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+           hover_name="country", log_x=True, size_max=60)
+fig.show()
+
+#%%
+import chart_studio
+import chart_studio.plotly as py
+
+#%%
+# username='deolgurpreet'
+# api_key='SlRW3lS2KG9slmE0XOMm'
+# chart_studio.tools.get_credentials_file(username=username,api_key=api_key)
+
+#%%
+py.plot(fig,filename='gdp_per_cap', auto_open=True)
